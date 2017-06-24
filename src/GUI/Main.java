64953ebc34384a12de33be1c8807217f3d2bdb95
airaketa <jk3000@yandex.ru>
2017-06-24 21:40:54 +0300

    deleted unnecessary fields

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,21 +1,12 @@
 package GUI;
 
-import Logic.EntryList;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Main extends Application {
-    private EntryList entryList;
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
